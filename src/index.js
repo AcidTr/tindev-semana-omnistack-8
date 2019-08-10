e5f7ed39d6e0558e2123bf52fe4c38a224b86aac
Victor Mourão <victor_murati@hotmail.com>
2019-08-10 13:08:27 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
+
+import Routes from './routes'
+
+
+function App () {
+  return (
+    <Routes/>
+  );
+};
+
+
+
+export default App;
